10c64380a86160a8627a6be9b6af6bef7434cd7f
yaseswini-128 <yaseswiniphalam@gmail.com>
2025-12-29 16:08:13 +0530

    Add files via upload

---
+++ merge sort.py
@@ -0,0 +1,31 @@
+def merge(arr,low,mid,high):
+    temp=[]
+    left,right=low,mid+1
+    while left<=mid and right<=high:
+        if arr[left]<=arr[right]:
+            temp.append(arr[left])
+            left+=1
+        else:
+            temp.append(arr[right])
+            right+=1
+    while left<=mid:
+        temp.append(arr[left])
+        left+=1
+    while right<=high:
+        temp.append(arr[right])
+        right+=1
+    for i in range(low,high+1):
+        arr[i]=temp[i-low]
+            
+def mergesort(arr,low,high):
+    if low>=high:
+        return
+    mid=(low+high)//2
+    mergesort(arr,low,mid)
+    mergesort(arr,mid+1,high)
+    merge(arr,low,mid,high)
+arr=(list(map(int,input().split())))
+low=0
+high=len(arr)-1
+mergesort(arr,low,high)
+print(arr)
